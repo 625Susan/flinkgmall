426c19a19fb03808b604b5ad459d1dcdee7bb102
susan <18983944245@163.com>
2024-05-28 09:27:50 +0800

    fix

--- gmall2022-realtime/src/main/java/com/xly/gmall/realtime/app/dws/DwsTradeSkuOrderWindow.java
+++ gmall2022-realtime/src/main/java/com/xly/gmall/realtime/app/dws/DwsTradeSkuOrderWindow.java
@@ -4,7 +4,7 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.xly.gmall.realtime.app.func.DimAsyncFunction;
 import com.xly.gmall.realtime.beans.TradeSkuOrderBean;
-import com.atguigu.gmall.realtime.utils.*;
+import com.xly.gmall.realtime.utils.*;
 import com.xly.gmall.realtime.utils.DataFormatUtil;
 import com.xly.gmall.realtime.utils.MyClickhouseUtil;
 import com.xly.gmall.realtime.utils.MyKafkaUtil;
